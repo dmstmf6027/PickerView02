a6e98d7615b311427472745ffc6223049a984218
201631053 <hubot@githhub@com>
2018-04-18 15:42:34 +0900

    1

--- PickerView02/ViewController.swift
+++ PickerView02/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var MyLabel: UILabel!
+    @IBOutlet weak var yourLabel: UILabel!
+    
+    var color = ["WHITE", "GREEN", "RED"]
+    var number = ["1", "2", "3", "4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // delegate와 ViewController 연결
+        
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 3
+        } else {
+            return 4
+        }
+        
+        
     }
 
-
+    //UIPickerViewDelegate method
+      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+        {
+            
+            if component == 0 {
+                return color[row]
+            } else {
+                return number[row]
+    }
 }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        if component == 0 {
+            MyLabel.text = color[row]
+        } else {
+            yourLabel.text = number[row] }
+    }
+}
+
 
